79ac48e556a88a31fe4e3cf55e9272d6790b7f9e
wjoo20 <juniorosis20@gmail.com>
2021-08-30 22:20:59 -0500

    Tarea Lab02

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      textValue: '',
+      count: 0,
+    };
+  }
+
+  changeTextInput = text => {
+    this.setState({
+      textValue: text
+    });
+  };
+
+  render(){
+    return (
+      <View style = {styles.container}>
+        
+        <Image source = {require('./assets/logo.png')} style = {styles.image}/>
+        <View>
+          <Text>Usuario: </Text>
+          <TextInput 
+            style = {styles.text}
+            onChangeText = {text => this.changeTextInput(text)}
+            value = {this.state.textValue}
+          />
+
+          <Text>Contraseña: </Text>
+          <TextInput 
+            style = {styles.text}
+            onChangeText = {text => this.changeTextInput(text)}
+            value = {this.state.textValue}
+          />
+        </View>       
+        <TouchableOpacity style = {styles.button} onPress = {this.onPress}>
+          <Text>Iniciar Sesión</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+  },
+
+  text: {
+    width: 280,
+    padding: 10,
+    height: 40, 
+    borderColor: 'gray', 
+    borderWidth: 1,
+  },
+
+  image: {
+    height: 200,
+    width: 250,
+  },
+
+  button: {
+    top: 50,
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
   },
 });
+
